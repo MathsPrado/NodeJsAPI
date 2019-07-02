70a99c262f07cf83f2365aa6e44845394d08894a
MathsPrado <matheusprado55@gmail.com>
2019-07-02 17:07:36 -0300

    bd conectado

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -4,10 +4,16 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const router = express.Router();
+const mongoose = require('mongoose');
+
+
+        //conecta ao banco
+mongoose.connect('mongodb+srv://YellowLemon:123@ndstr-n1rdl.mongodb.net/test?retryWrites=true&w=majority');
 
         //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
